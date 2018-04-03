a08836cd108e4f8681204591ca1f66c502fa05a2
Justin Dennison <justin1dennison@gmail.com>
2018-04-03 18:36:52 -0400

    created Home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
+//Components
+import Home from './components/Home';
 class App extends Component {
   render() {
     return (
       <div>
         Hello
+        <Home />
       </div>
     );
   }
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = props => (
+    <div>
+        This is your home.
+    </div>
+)
+
+export default Home;
