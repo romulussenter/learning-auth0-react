622cded8494ed150d0f07e9b8d29b33af2ff3797
Justin Dennison <justin1dennison@gmail.com>
2018-04-03 18:49:07 -0400

    set up router and routes with navigation to each respective component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Route, Link } from 'react-router-dom';
+
 
 //Components
 import Home from './components/Home';
@@ -9,9 +11,16 @@ class App extends Component {
   render() {
     return (
       <div>
-        Hello
-        <Home />
-        <Profile />
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/profile'>Profile</Link>
+          </li>
+        </ul>
+        <Route path='/' exact component={Home} />
+        <Route path='/profile' exact component={Profile} />
       </div>
     );
   }
