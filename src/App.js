d176d6217d9e6d637ae38ccb35c9c52e7996909f
Justin Dennison <justin1dennison@gmail.com>
2018-04-03 20:46:28 -0400

    created callback and wired up the handle authentication

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 import { Route, Link } from 'react-router-dom';
 import Auth from './services/auth';
 
+
 //Components
 import Home from './components/Home';
 import Profile from './components/Profile';
+import Callback from './components/Callback';
 
 class App extends Component {
   constructor(){
@@ -13,6 +15,11 @@ class App extends Component {
     this.auth = new Auth();
   }
   
+  handleAuthentication = (nextState, replace) => {
+    if (/access_token|id_token|error/.test(nextState.location.hash)) {
+      this.auth.handleAuthentication();
+    }
+  }
   render() {
     return (
       <div>
@@ -27,6 +34,10 @@ class App extends Component {
         </ul>
         <Route path='/' exact component={Home} />
         <Route path='/profile' exact component={Profile} />
+        <Route path='/callback' exact render={(props) => {
+          this.handleAuthentication(props);
+          return <Callback {...props} />
+        }} />
       </div>
     );
   }
