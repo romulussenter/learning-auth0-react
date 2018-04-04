f64f4e665737e4776759c8407cdfd426eec18066
Justin Dennison <justin1dennison@gmail.com>
2018-04-03 20:03:18 -0400

    finished the auth service and ensured the auth and router had shared histories

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import { Route, Link } from 'react-router-dom';
 import Auth from './services/auth';
 
-
 //Components
 import Home from './components/Home';
 import Profile from './components/Profile';
@@ -13,9 +12,7 @@ class App extends Component {
     super();
     this.auth = new Auth();
   }
-  componentDidMount(){
-    this.auth.login();
-  }
+  
   render() {
     return (
       <div>
@@ -26,6 +23,7 @@ class App extends Component {
           <li>
             <Link to='/profile'>Profile</Link>
           </li>
+          <li><button onClick={() => this.auth.login()}>Login</button></li>
         </ul>
         <Route path='/' exact component={Home} />
         <Route path='/profile' exact component={Profile} />
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import { BrowserRouter as Router } from "react-router-dom";
+import { Router } from "react-router-dom";
+import history from './history';
 
 ReactDOM.render(
-  <Router>
+  <Router history={history}>
     <App />
   </Router>,
   document.getElementById("root")
